48114318d54b01017db0b816b56de75d8afb1fc8
AWS ParallelCluster user <ec2-user@ip-10-0-6-201.ap-southeast-1.compute.internal>
2026-09-06 05:30:13 +0000

    Add just-in-time job submission scripts (Lab 5, Part A, Section 2)

---
+++ rplhighpass-sort-slurm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Submit this script with: sbatch <this-filename>
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=1   # number of CPUs for this task
+#SBATCH -J "hps"   # job name
+## /SBATCH -p general # partition (queue)
+#SBATCH -o rplhps-slurm.%N.%j.out # STDOUT
+#SBATCH -e rplhps-slurm.%N.%j.err # STDERR
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+source /data/miniconda3/etc/profile.d/conda.sh
+conda activate env1
+python -u -c "import PyHipp as pyh; \
+import time; \
+pyh.RPLHighPass(saveLevel=1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
+print(time.localtime());"
