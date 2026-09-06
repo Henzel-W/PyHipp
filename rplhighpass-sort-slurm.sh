3d012d0889327e5104ca14b5ed2004f5558a68d9
AWS ParallelCluster user <ec2-user@ip-10-0-6-201.ap-southeast-1.compute.internal>
2026-09-06 14:54:34 +0000

    Wire envlist.py conda environment pool into rplhighpass-sort-slurm.sh (Lab 5, Part A, Section 3)

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Submit this script with: sbatch <this-filename>
-#SBATCH --time=24:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --cpus-per-task=1   # number of CPUs for this task
-#SBATCH -J "hps"   # job name
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH -J "hps"
 ## /SBATCH -p general # partition (queue)
 #SBATCH -o rplhps-slurm.%N.%j.out # STDOUT
 #SBATCH -e rplhps-slurm.%N.%j.err # STDERR
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 source /data/miniconda3/etc/profile.d/conda.sh
 conda activate env1
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -19,3 +23,5 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
