d0b3131c979c18e4b233dfe39b90396435161926
AWS ParallelCluster user <ec2-user@ip-10-0-15-146.ap-southeast-1.compute.internal>
2026-09-12 12:54:16 +0000

    Local edits before merging upstream

--- rplparallel-slurm.sh
+++ rplparallel-slurm.sh
@@ -29,4 +29,4 @@ pyh.raycast(1); \
 print(time.localtime()); \
 print(time.time()-t0);"
 
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:789223369126:awsnotify --message "RPLParallelJobDone"
+#aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:789223369126:awsnotify --message "RPLParallelJobDone"
